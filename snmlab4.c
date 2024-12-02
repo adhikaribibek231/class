ce703259057c542b7da25dae5cbe0f041bf866e9
adhikaribibek231 <adhikaribibek231@gmail.com>
2024-12-02 12:20:52 +0545

    more

--- snmlab4.c
+++ snmlab4.c
@@ -1,3 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#define max 200;
+float x[max] ,y[max];
+void create_randomX(int r,int N){
+    int i;
+    for (i=0;i<N;i++){
+        x[i]=(float)rand()/(float)(RAND_MAX/r);
+    }
+}
